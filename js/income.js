a1eb00682bd24ee52a7d5f8416d84ea2b74fb0c1
fibernardo <fbernardo@addu.edu.ph>
2024-12-07 23:46:53 +0800

    Update income.js

--- js/income.js
+++ js/income.js
@@ -1,25 +1,25 @@
 document.querySelector("#calculateTax").addEventListener("click", () => {
-    const taxableIncome = document.querySelector("#taxableIncome").value;
+    const taxableIncome = document.querySelector("#tIncome").value;
     const calculatedTax = ((num1) => {
         const taxableIncome = parseFloat(num1);
         let taxAmount = 0;
 
-        if (taxableIncome <= 250000) {
-            taxAmount = 0;
-        } else if (taxableIncome <= 400000) {
-            taxAmount = (taxableIncome - 250000) * 0.20;
-        } else if (taxableIncome <= 800000) {
-            taxAmount = (taxableIncome - 400000) * 0.25 + 30000;
-        } else if (taxableIncome <= 2000000) {
-            taxAmount = (taxableIncome - 800000) * 0.30 + 130000;
-        } else if (taxableIncome <= 8000000) {
-            taxAmount = (taxableIncome - 2000000) * 0.32 + 490000;
+        if (tIncome <= 250000) {
+            tAmount = 0;
+        } else if (tIncome <= 400000) {
+            tAmount = (tIncome - 250000) * 0.20;
+        } else if (tIncome <= 800000) {
+            tAmount = (tIncome - 400000) * 0.25 + 30000;
+        } else if (tIncome <= 2000000) {
+            tAmount = (tIncome - 800000) * 0.30 + 130000;
+        } else if (tIncome <= 8000000) {
+            tAmount = (tIncome - 2000000) * 0.32 + 490000;
         } else {
-            taxAmount = (taxableIncome - 8000000) * 0.35 + 2410000;
+            tAmount = (tIncome - 8000000) * 0.35 + 2410000;
         }
 
-        return taxAmount;
-    })(taxableIncome);
+        return tAmount;
+    })(tIncome);
 
     document.querySelector("#taxResult").textContent = `Total Tax: ₱${calculatedTax.toLocaleString(undefined, { minimumFractionDigits: 2, maximumFractionDigits: 2 })}`;
 });
